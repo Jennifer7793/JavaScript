ceef611757d03f01a2133f41e2263922e7dc584b
LAPTOP-56NU4RUS\jenni <jennifer7793@gmail.com>
2021-11-12 17:14:51 +0800

    1112save

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,17 @@
-class UsersController < ActionController::Base
+class UsersController < ApplicationController
   def sign_up
+    @user = User.new
   end
+
   def account_verify
-    redirect_to "/"
+    clean_params = params.require(:user).permit(:username, :password, :email)
+
+    @user = User.new(clean_params)
+
+    if @user.save
+     redirect_to "/"
+    else
+     render :sign_up 
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  get "/", to: "pages#index"
-  # root "pages#index" 另種寫法
+  root "pages#index" 
+
   get "/about", to: "pages#about"
 
   get "/sign_up", to: "users#sign_up"
-  post "account_verify", to: "users#account_verify"
+  post "/account_verify", to: "users#accout_verify"
 end
