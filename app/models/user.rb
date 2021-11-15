19f8c055d00fccccca357240e5270a76e2f764ee
LAPTOP-56NU4RUS\jenni <jennifer7793@gmail.com>
2021-11-15 15:02:12 +0800

    b4_create

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
 class User < ApplicationRecord
-  validates :username, presence: true
+  validate :email, :username, presence: true
+
+  before_save :say_hello
+
+  private
+  def say_hello
+    p "-" *30
+    p "hi"
+    p "-" *30
 end
