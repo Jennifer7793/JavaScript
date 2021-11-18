8938b081e0f48c12a301c2345502d64901339d06
LAPTOP-56NU4RUS\jenni <jennifer7793@gmail.com>
2021-11-18 18:27:13 +0800

    update_user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,20 @@
 class User < ApplicationRecord
   validates :email, :username, presence: true
   
-  # before_create :encrypt_password
+  before_create :encrypt_password
 
-  # private
-  # def encrypt_password
-  #   self.password = 
+  def self.login(user_info)
+    email = user_info[:email]
+
+    salted_password = "xy#{passowrd.reverse}hellohey"
+    encryted_password = Digest::SHA1.hexidigest(salted_password)
+
+    self.find_by(email: email, password: encryted_password)
+  end
+
+  private
+  def encrypt_password
+    self.password = "xy#{self.password.reverse}hellohey"
+    self.password = Digest::SHA1.hexidigest(salted_password)
+  end
 end
