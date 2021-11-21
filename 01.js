cf9adcf7349ccb63d5896b8cbdb74a6bfb1cd2fc
LAPTOP-56NU4RUS\jenni <jennifer7793@gmail.com>
2021-11-21 17:10:06 +0800

    discounted

--- 01.js
+++ 01.js
@@ -1,9 +1,15 @@
-let total = 100;
+let total = 700;
+let discounted = 0;
 
 if (total > 1000) {
-  console.log(total * 0.7);
-} else if (total > 500) {
-  console.log(total * 0.8);
+  discounted = total * 0.7
+} else if (total > 5000) {
+  discounted = total * 0.75
+} else if (total > 500)
+  discounted = total * 0.8
 } else {
-  console.log(total);
+  discounted = total
 }
+
+console.log(discounted)
+
