fae3a85bbe40121a36849b4e863b2e4c4a608b41
LAPTOP-56NU4RUS\jenni <jennifer7793@gmail.com>
2021-11-21 17:12:53 +0800

    discount_to_rate

--- 01.js
+++ 01.js
@@ -1,15 +1,14 @@
 let total = 700;
-let discounted = 0;
+let rate = 0;
 
 if (total > 1000) {
-  discounted = total * 0.7
+  rate = 0.7
 } else if (total > 5000) {
-  discounted = total * 0.75
+  rate = 0.75
 } else if (total > 500)
-  discounted = total * 0.8
-} else {
-  discounted = total
-}
+  rate = 0.8
+} 
 
+var discounted = total * rate
 console.log(discounted)
 
