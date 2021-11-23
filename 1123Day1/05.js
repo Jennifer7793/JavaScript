b37924aa4343b191da981274714538be6571f7cd
LAPTOP-56NU4RUS\jenni <jennifer7793@gmail.com>
2021-11-23 15:56:37 +0800

    nd_search_methods

--- 1123Day1/05.js
+++ 1123Day1/05.js
@@ -1,7 +1,21 @@
 const list = [1, 2, 3, 4, 5]
 
-const newArr = list.map(x => x * 2)
+// 箭頭函數 Arrow Function
+const newArr = list.map(x => x * 2) 
 console.log(newArr)
+
+// 匿名函數 Anonymous Function
+const result = list.map(function (x) {
+  return 2 * x;
+})
+
+console.log(result)
+
+function double(n) {
+  n * 2;
+}
+console.log(result);
+
 // const result = [];
 // for (let i = 0; i < 5; i++) {
 //   result.push(list[i] * 2);
---
+++ 1123Day1/06.js
@@ -0,0 +1,7 @@
+const list = [1, 2, 3, 4, 5]
+
+const result = list.reduce(function (x) {
+  return var(x = 0, x < 5, x++)
+})
+
+console.log(result)
