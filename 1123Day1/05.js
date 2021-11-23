e3a828d79400220aaf3a529f8e89dddc00fc0952
Jennifer <jennifer7793@gmail.com>
2021-11-23 15:45:09 +0800

    map

--- 1123Day1/05.js
+++ 1123Day1/05.js
@@ -1,8 +1,10 @@
 const list = [1, 2, 3, 4, 5]
 
-const result = [];
-for (let i = 0; i < 5; i++) {
-  result.push(list[i] * 2);
-}
+const newArr = list.map(x => x * 2)
+console.log(newArr)
+// const result = [];
+// for (let i = 0; i < 5; i++) {
+//   result.push(list[i] * 2);
+// }
 
-console.log(result); 
+// console.log(result); 
