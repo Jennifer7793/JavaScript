8a11aae5253e47d9c6f76eb0a90479d3d946dbf3
Jennifer <jennifer7793@gmail.com>
2021-11-25 15:41:55 +0800

    preventDefault

---
+++ 1125Day2/script.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function() {
+  const btn = document.querySelector("#btn");
+
+  btn.addEventListener("click", function(e) {
+    e.preventDefault()''
+    console.log("go!");
+  })
+
+  // const txt = document.getElementById('hi')
+  // txt[].innerText = "World";
+  // }
+  // const ABC = document.getElementsByClassName('hi')
+  // for (var a = 0; a < 3; a = a+1 ) {
+  // ABC[a].innerText = "Z";
+  // }
+})
