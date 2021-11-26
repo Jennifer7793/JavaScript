1eafd2d47037d6fa8051e40d964a2aa602ec266e
LAPTOP-56NU4RUS\jenni <jennifer7793@gmail.com>
2021-11-26 08:13:32 +0800

    save_to_1540

--- 1125Day2/07.js
+++ 1125Day2/07.js
@@ -2,8 +2,7 @@ document.addEventListener("DOMContentLoaded", function() {
   const sel_all = document.querySelector("#selectAll");
   const cbox = document.querySelector('.check');
   sall.addEventListener("click",
-  for (let i=0,i<3,i++) {
-      cbox.checked == true
+  
     }
   )
 })
