c737c615ed66130bc99181938bde89deea7fa866
Jennifer <jennifer7793@gmail.com>
2021-11-27 13:42:19 +0800

    p19.20

---
+++ Practice/1127/p19.js
@@ -0,0 +1,8 @@
+var amount = 99.99;
+amount = amount * 2;
+
+console.log(amount);
+
+//將amount轉成字串並在最前面加上$
+amount = "$" + String(amount);
+console.log(amount);
---
+++ Practice/1127/p20.js
@@ -0,0 +1,7 @@
+var TAX_RATE = 0.08;
+var amount = 99.99;
+amount = amount * 2;
+amount = amount + (amount * TAX_RATE);
+
+console.log(amount);
+console.log(amount.toFixed(2));
