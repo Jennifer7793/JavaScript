676748d2f5f639726a9da25af9c5dccd20f136ad
Jennifer <jennifer7793@gmail.com>
2021-11-30 14:18:29 +0800

    jq_cl(123)

---
+++ 1130Day4/jq.js
@@ -0,0 +1,13 @@
+$().ready(() => {
+  $(".hello").on("click", () => {
+    console.log(123);
+  });
+});
+
+// document.addEventListener("DOMContentLoaded", function() {
+//   const press = document.querySelector(".hello");
+//    press.addEventListener ("click", (e) => {
+//     console.log(123);
+//   })
+// });
+
