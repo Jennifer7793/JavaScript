dac4a8c4b20ae5e5dd63b3f3fda61a88e0264b47
Jennifer <jennifer7793@gmail.com>
2021-11-30 15:09:05 +0800

    jcarousel_not_done

--- 1130Day4/jq.js
+++ 1130Day4/jq.js
@@ -1,8 +1,29 @@
-$().ready(() => {
-  $(".hello").on("click", () => {
-    console.log(123);
+$(function() {
+  $('.jcarousel').jcarousel({
+  });
+
+  $('.jcarousel').jcarousel('scroll', '+=2');
+
+  var instance = $('.jcarousel').data('jcarousel');
+
+  instance.scroll('+=2');
+
+  $('.jcarousel').jcarousel('scroll', btn);
+
+  $('.jcarousel-prev').click(function() {
+    $('.jcarousel').jcarousel('scroll', '-=1');
+  });
+
+  $('.jcarousel-next').click(function() {
+      $('.jcarousel').jcarousel('scroll', '+=1');
   });
 });
+
+// $().ready(() => {
+//   $(".hello").on("click", () => {
+//     console.log(123);
+//   });
+// });
 
 // document.addEventListener("DOMContentLoaded", function() {
 //   const press = document.querySelector(".hello");
