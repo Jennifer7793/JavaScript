93e0d97a4430f5a987d0c3f0c8ea5606deb25ee4
Jennifer <jennifer7793@gmail.com>
2021-12-02 15:13:49 +0800

    02_click_cl

---
+++ Practice/1202/01_localStorage.js
@@ -0,0 +1,22 @@
+// myStorage = window.localStorage;
+
+// localStorage.setItem('myCat', 'Tom');
+// var cat = localStorage.getItem('Tom');
+// console.log(cat);
+
+// // 將資料存到sessionStorage
+// sessionStorage.setItem('key', 'value');
+
+// // 從sessionStorage取得之前存的資料
+// var data = sessionStorage.getItem('key');
+
+// // 從sessionStorage移除之前存的資料
+// sessionStorage.removeItem('key');
+
+// // 從sessionStorage移除之前存的所有資料
+// sessionStorage.clear();
+
+
+document.cookie = "yummy_cookie=choco";
+document.cookie = "tasty_cookie=strawberry";
+console.log(document.cookie);
---
+++ Practice/1202/02.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", ()=> {
+  const add = document.querySelector(".add");
+
+  add.addEventListener("click", () => {
+    console.log("Heyyy");
+  })
+});
