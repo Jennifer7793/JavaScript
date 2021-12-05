a56e4624b7de4ff133694617317378f03b6aee05
Jennifer <jennifer7793@gmail.com>
2021-12-05 23:39:29 +0800

    var_slice

---
+++ Practice/1205/var.js
@@ -0,0 +1,21 @@
+var someVar = 5;
+someOtherVar = 10;
+var someThirdVar;
+var someFourthVar = 2, someFifthVar = 4;
+
+someVar += 5;
+someVar *= 10;
+someVar++;
+someVar--;
+
+var myArray = ["Hello", 45, true];
+myArray.push("World");
+myArray[3] = "Hello";
+myArray.unshift(3);
+someVar = myArray.pop();
+
+var myArray0 = [32,false,"js",12,56,90];
+myArray0.join(";");
+myArray0.slice(1,4);
+console.log(myArray0.slice(1,4));
+console.log(myArray0.join(";"));
