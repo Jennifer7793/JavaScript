312a435761e2c60926d52ccd4b05fd0200c07dee
JenJen <jennifer7793@gmail.com>
2022-02-02 13:01:06 +0800

    JS parseInt

---
+++ 0202parseInt.js
@@ -0,0 +1,6 @@
+console.log(parseInt(0.5)); //0
+console.log(parseInt(0.00000005)); //5
+console.log(parseInt(0.000000005)); //5
+console.log(parseInt(0.000000012)); //1
+console.log(parseInt(0.0000000123)); //1
+console.log(parseInt(1.5555555)); //1
