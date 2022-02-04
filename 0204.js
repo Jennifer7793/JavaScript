3aa0421bbaa3fffa4ea60dff67e65373df288e91
JenJen <jennifer7793@gmail.com>
2022-02-04 16:32:04 +0800

    euqal and d colon

---
+++ 0204.js
@@ -0,0 +1,18 @@
+// var name = "Ken"
+// var new_name = { name: "Jennifer" }
+// console.log(name);
+// console.log(new_name)
+
+let name = "interviewgogo";
+name.a = "Ken";
+name.b = "Seth";
+name.c = "Jimmy";
+
+let new_name = {
+  "a": "Ken",
+  "b": "Seth",
+  "c": "Jimmy"
+};
+
+console.log(name);
+console.log(new_name);
