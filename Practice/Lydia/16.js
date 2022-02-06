bf7af9fdf90e0eb51949e6a7f28372cb5b4ea59d
JenJen <jennifer7793@gmail.com>
2022-02-06 15:32:51 +0800

    Lydia to Q18

--- Practice/Lydia/16.js
+++ Practice/Lydia/16.js
@@ -1,5 +1,5 @@
 let number = 0;
 
-console.log(number++); //印出結果再+
-console.log(++number);
-console.log(number);
+console.log(number++); //印出結果再++所以是0
+console.log(++number); //2
+console.log(number); //2
---
+++ Practice/Lydia/17.js
@@ -0,0 +1,14 @@
+function getPersonInfo(one, two, three) {
+  console.log(one);
+  console.log(two);
+  console.log(three);
+}
+
+const person = "Lydia";
+const age = 21;
+
+getPersonInfo`${person} is ${age} years old`
+
+//[ '', ' is ', ' years old' ]
+//Lydia
+//21
