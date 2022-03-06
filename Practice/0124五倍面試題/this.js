ee91132c11b4ae8db40d768f7b7462132485d837
LAPTOP-56NU4RUS\jenni <jennifer7793@gmail.com>
2022-03-06 11:50:18 +0800

    add answer and explaination

--- Practice/0124五倍面試題/this.js
+++ Practice/0124五倍面試題/this.js
@@ -2,21 +2,21 @@ var name = "悟空"
 
 var hero = {
   name: "悟空",
-  sayMyName: function () { console.log(this.name); }
-}
+  sayMyName: function() { console.log(this.name); }
+};
 
-hero.sayMyName();
+hero.sayMyName(); //悟空 **誰呼叫函式，誰就是this
 
 const there = () => {
   var name = "Hello";
   console.log(this.name);
 }
 
-there();
+there(); //undefined **箭頭函式沒有自己的this, 綁訂到其定義時所在的物件，指向window全域
 
 function here() {
   var name = "達爾";
   console.log(this.name);
 }
 
-here();
+here(); //undefined **屬於直接呼叫的函式，所以this指向全域
