4c1d3d0c2ef47f54afec21c975b516b149ca65bf
JenJen <jennifer7793@gmail.com>
2022-03-18 16:49:31 +0800

    use default to give default value when there's null

--- Practice/conditional_operator.js
+++ Practice/conditional_operator.js
@@ -11,3 +11,11 @@ console.log(Jennifer(null));
 var age = 20
 var beverage = (age >= 18 ) ? "Beer" : "Juice";
 console.log(beverage);
+
+let greeting = person => {
+  let name = person ? person.name : `Welcome`
+  return `Hello, ${name}`
+}
+
+console.log(greeting({name: `Jennifer`}));
+console.log(greeting());
