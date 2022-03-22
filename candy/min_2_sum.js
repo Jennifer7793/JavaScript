65085dcc5cf9bd0a7837cf9109af311c5e8cde85
JenJen <jennifer7793@gmail.com>
2022-03-22 08:57:32 +0800

    use sort and sum up index[0] + index [1]

--- candy/min_2_sum.js
+++ candy/min_2_sum.js
@@ -4,9 +4,22 @@
 //   [15, 28, 4, 2, 43] 印出 6
 //   [23, 71, 33, 82, 1] 印出 24
 
+const arr = [23, 71, 33, 82, 1]
+const exp = [15, 28, 4, 2, 43]
 const list = [19, 5, 42, 2, 77]
 // 實作程式碼寫在這裡
-list.sort();
-console.log(list);
+list.sort(function(a, b) {
+  return a - b;
+});
+console.log(list[0] + list[1]);
 
-// 最終結果應該要印出
+// 最終結果應該要印出7
+arr.sort(function(a, b) {
+  return a - b;
+});
+console.log(arr[0] + arr[1]);
+
+exp.sort(function(a, b) {
+  return a - b;
+});
+console.log(exp[0] + exp[1]);
