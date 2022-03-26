60a46ee3640207434a451d35c6607991bf2e9068
JenJen <jennifer7793@gmail.com>
2022-03-26 23:23:50 +0800

    method: slice

---
+++ Practice/slice.js
@@ -0,0 +1,19 @@
+const engineer = ['Ken', 'Seth', 'Lilith', 'Roy', 'Liz', 'Jen'];
+
+console.log(engineer.slice(2)); //['Lilith, 'Roy', 'Liz', 'Jen']
+// 以index[2]為開始，印出後面的值
+
+console.log(engineer.slice(2, 4)); //['Lilith', 'Roy']
+// 以index[2]為開始，印到第4個值
+
+console.log(engineer.slice(1, 5)); //['Seth', 'Lilith', 'Roy', 'Liz']
+// 以index[1]為開始，印到第5個值
+
+console.log(engineer.slice(-2)); //['Liz', 'Jen']
+// 以index[-2]為開始，逐一印出後面的值
+
+console.log(engineer.slice(2, -1)); //['Lilith', 'Roy', 'Liz']
+// 以index[2]為開始，
+
+console.log(engineer.slice()); //['Ken', 'Seth', 'Lilith', 'Roy', 'Liz', 'Jen']
+// 印出原本的陣列
