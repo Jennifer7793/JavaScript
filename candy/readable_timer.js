987942fafd2770ce0caa0a4af4f60c59150b154d
JenJen <jennifer7793@gmail.com>
2022-03-26 11:29:14 +0800

    candy readable_timer, need to readjust the result

---
+++ candy/readable_timer.js
@@ -0,0 +1,27 @@
+function humanReadableTimer(seconds) {
+  // const result = new Date(seconds * 1000).toISOString().slice(11, 19);
+  // console.log(result);
+    var hr = Math.floor(seconds/3600)
+    var min = Math.round(Math.floor(seconds % 3600)/60)
+    var sec = ((seconds % 3600) % 60)
+  return  `${hr}:${min}:${sec}`   
+}
+
+// function humanReadableTimer(seconds) {
+//   var hrs = seconds.getHours().toString().padStart(2, '0')
+//   var mins = seconds.getMinutes().toString().padStart(2, '0')
+//   var secs = seconds.getSeconds().toString().padStart(2, '0')
+
+//   return `${hrs}:${mins}:${secs}`
+// }
+
+console.log(humanReadableTimer(0))      // 印出 00:00:00
+console.log(humanReadableTimer(59))     // 印出 00:00:59
+console.log(humanReadableTimer(60))     // 印出 00:01:00
+console.log(humanReadableTimer(90))     // 印出 00:01:30
+console.log(humanReadableTimer(3599))   // 印出 00:59:59
+console.log(humanReadableTimer(3600))   // 印出 01:00:00
+console.log(humanReadableTimer(45296))  // 印出 12:34:56
+console.log(humanReadableTimer(86399))  // 印出 23:59:59
+console.log(humanReadableTimer(86400))  // 印出 24:00:00
+console.log(humanReadableTimer(359999)) // 印出 99:59:59
