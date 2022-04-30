d2b790ec4eaf84eba44b9d275f8a4a1f9c5117ae
JenJen <jennifer7793@gmail.com>
2022-04-30 21:21:39 +0800

    promise

--- Practice/promise.js
+++ Practice/promise.js
@@ -17,3 +17,33 @@ promise()
   }, (fail) => {
     console.log(fail);
   });
+
+~async function { //~can direct execute the function
+  const delay = (s) => {
+    return new Promise(function(resolve){
+      setTimeout(resolve, s);
+    });
+  };
+
+  console.log(1);
+  await delay(1000);
+  console.log(2);
+  await delay(2000)
+  console.log(3);
+}(); //with ending () to call function 
+
+  // const delay = (s) => {
+  //   return new Promise(resolve => {
+  //     setTimeout(resolve, s);
+  //   });
+  // };
+
+  // delay().then(() => {
+  //   console.log(1);
+  //   return delay(1000);
+  // }).then(() => {
+  //   console.log(2);
+  //   return delay(2000);
+  // }).then(()=> {
+  //   console.log(3);
+  // })
