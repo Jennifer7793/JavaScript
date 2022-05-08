1540702c72b2136377eb4be0c15b9bc4fd77c5c9
JenJen <jennifer7793@gmail.com>
2022-05-08 12:35:59 +0800

    if switch w/o break will continuing executing

--- diff_nodejs/switch.js
+++ diff_nodejs/switch.js
@@ -1,11 +1,23 @@
 var day = new Date().getDay();
+// switch (day) {
+//   case 6:
+//     console.log('Today is Sat so is off day');
+//     break;
+//   case 0:
+//     console.log('It\'s Sun is off day');
+//     break;
+//   default:
+//     console.log('Today is weekday so no off');
+// }
+
+// ---- below if took away break, it will continue executing. ----
 switch (day) {
   case 6:
     console.log('Today is Sat so is off day');
     break;
   case 0:
     console.log('It\'s Sun is off day');
-    break;
+  //  break;
   default:
     console.log('Today is weekday so no off');
 }
