40e0bd013add828d8b76bb7fd16c09b53b76474a
JenJen <jennifer7793@gmail.com>
2022-05-08 12:38:02 +0800

    switch shorter form to check off day or not

--- diff_nodejs/switch.js
+++ diff_nodejs/switch.js
@@ -1,4 +1,13 @@
 var day = new Date().getDay();
+switch (day) {
+  case 6:
+  case 0:
+    console.log('It\'s weekend so is off day');
+    break;
+  default:
+    console.log('Today is weekday so no off');
+}
+
 // switch (day) {
 //   case 6:
 //     console.log('Today is Sat so is off day');
@@ -11,13 +20,13 @@ var day = new Date().getDay();
 // }
 
 // ---- below if took away break, it will continue executing. ----
-switch (day) {
-  case 6:
-    console.log('Today is Sat so is off day');
-    break;
-  case 0:
-    console.log('It\'s Sun is off day');
-  //  break;
-  default:
-    console.log('Today is weekday so no off');
-}
+// switch (day) {
+//   case 6:
+//     console.log('Today is Sat so is off day');
+//     break;
+//   case 0:
+//     console.log('It\'s Sun is off day');
+//   //  break;
+//   default:
+//     console.log('Today is weekday so no off');
+// }
