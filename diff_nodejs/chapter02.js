2bd921702f4d4421386d15b46c766eb348143b25
JenJen <jennifer7793@gmail.com>
2022-05-08 10:30:05 +0800

    checking false, null, undefined, space and 0

--- diff_nodejs/chapter02.js
+++ diff_nodejs/chapter02.js
@@ -16,3 +16,14 @@ console.log(StringObjectRefA === StringObjectRefB); //false
 
 console.log(typeof(StringRefA)); //string
 console.log(StringRefA === string); //true
+
+// 以下易判斷錯誤
+console.log("0" == ""); //false
+console.log("0" == " "); //true
+console.log(0 == "0"); //true!!!
+console.log(false == "false"); //false
+console.log(false == "0"); //true!!!
+console.log(false == undefined); //false
+console.log(false == null); //false
+console.log(null == undefined); //true!!!
+console.log(" \t\r\n" == 0); //true!!!
